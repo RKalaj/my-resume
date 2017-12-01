8ecd465007353afc859c1576e06f96df611fb7bb
Robert Kalaj <rkalaj@udel.edu>
2017-11-30 23:57:53 -0500

    added profs, add and edit forms

--- manage.py
+++ manage.py
@@ -1,7 +1,29 @@
 from flask_script import Manager
-from myresume import app
+from myresume import app, db, Courses, Prof
 
 manager = Manager(app)
+
+@manager.command
+def deploy():
+    db.drop_all()
+    db.create_all()
+    harry = Prof(name='Harry Wang', department='MIS')
+    junbo = Prof(name='Junbo Son', department='MIS')
+    jillian = Prof(name='Jillian Pratt', department='ACCT')
+    susan = Prof(name='Susan Murphy', department='OM')
+    misy350 = Courses(number='MISY350', title='Business Application Development II', desc='Covers concepts related to client side development, including cascading style sheets and JavaScript.', prof=harry)
+    buad345 = Courses(number='BUAD345', title='Decision Analytics and Visualization', desc='Analytics leverages both the proliferation of data and the advancement of computational tools to bring a new level of sophistication to business decision making. As part of developing an analytic mind and skillset, this course teaches students to properly frame decision problems, represent and understand how to manage uncertainty inherent in those problems, manipulate large data sets using modern software to prescribe recommended actions, and to then compel organizational change through data visualizations.', prof=junbo)
+    acct352 = Courses(number='ACCT352', title='Law and Social Issues in Business', desc='Focuses on the legal environment of business, including objectives of the law, sources of the law, regulatory and judicial process, and effect of government and society on the formation and evolution of the law.', prof=junbo)
+    buad306 = Courses(number='BUAD306', title='Service and Operations Management', desc='Analysis of major problems faced by operations managers at different levels of management. Topics include scheduling, forecasting, process design, inventory management and quality management.', prof=junbo)
+    db.session.add(misy350)
+    db.session.add(buad345)
+    db.session.add(acct352)
+    db.session.add(buad306)
+    db.session.add(harry)
+    db.session.add(junbo)
+    db.session.add(jillian)
+    db.session.add(susan)
+    db.session.commit()
 
 
 if __name__=='__main__':
--- myresume.py
+++ myresume.py
@@ -1,5 +1,31 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, session, render_template, request, flash, redirect, url_for, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess secure key'
+
+# setup SQLAlchemy
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+db = SQLAlchemy(app)
+
+
+class Courses(db.Model):
+    __tablename__ = 'courses'
+    id = db.Column(db.Integer, primary_key=True)
+    number = db.Column(db.String(256))
+    title = db.Column(db.String(256))
+    desc = db.Column(db.String(256))
+    prof_id = db.Column(db.Integer, db.ForeignKey('prof.id'))
+
+
+class Prof(db.Model):
+    __tablename__ = 'prof'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    department = db.Column(db.Text)
+    courses = db.relationship('Courses', backref='prof', cascade="delete")
 
 
 @app.route('/')
@@ -10,25 +36,89 @@ def home():
 
 @app.route('/courses')
 def get_courses():
-    courses = {
-        'MISY350',
-        'BUAD345',
-        'ACCT352'
-    }
-    message = {
-        'Business Application Development II',
-        'Decision Analytics and Visualization',
-        'Law and Social Issues in Business'
-    }
+    courses = Courses.query.all()
     return render_template('courses.html', courses=courses)
-    return render_template('courses.html', message=message)
 
+@app.route('/profs')
+def get_prof():
+    prof = Prof.query.all()
+    return render_template('profs.html', prof=prof)
 
 
 @app.route('/about')
 def get_about():
     # return '<h1>hello %s your age is %d</h1>' % (name, 3)
     return render_template('about.html')
+
+
+@app.route('/profs/add', methods=['GET', 'POST'])
+def add_prof():
+    if request.method == 'GET':
+        return render_template('profs-add.html')
+    if request.method == 'POST':
+        # get data from the form
+        name = request.form['name']
+        department = request.form['department']
+
+        # insert the data into the database
+        prof = Prof(name=name, department=department)
+        db.session.add(prof)
+        db.session.commit()
+        return redirect(url_for('get_prof'))
+
+
+@app.route('/profs/edit/<int:id>', methods=['GET', 'POST'])
+def edit_prof(id):
+    prof = Prof.query.filter_by(id=id).first()
+    if request.method == 'GET':
+        return render_template('profs-edit.html', prof=prof)
+    if request.method == 'POST':
+        # update data based on the form data
+        prof.name = request.form['name']
+        prof.department = request.form['department']
+        # update the database
+        db.session.commit()
+        return redirect(url_for('get_prof'))
+
+
+@app.route('/courses/add', methods=['GET', 'POST'])
+def add_courses():
+    prof = Prof.query.all()
+    if request.method == 'GET':
+        prof = Prof.query.all()
+        return render_template('courses-add.html', prof=prof)
+    if request.method == 'POST':
+        # get data from the form
+        number = request.form['number']
+        title = request.form['title']
+        desc = request.form['desc']
+        prof_name = request.form['prof']
+        prof = Prof.query.filter_by(name=prof_name).first()
+        courses = Courses(number=number, title=title, desc=desc, prof=prof)
+
+        # insert the data into the database
+        db.session.add(courses)
+        db.session.commit()
+        return redirect(url_for('get_courses'))
+
+
+@app.route('/courses/edit/<int:id>', methods=['GET', 'POST'])
+def edit_courses(id):
+    courses = Courses.query.filter_by(id=id).first()
+    prof = Prof.query.all()
+    if request.method == 'GET':
+        return render_template('courses-edit.html', courses=courses, prof=prof)
+    if request.method == 'POST':
+        # update data based on the form data
+        courses.number = request.form['number']
+        courses.title = request.form['title']
+        courses.desc = request.form['desc']
+        prof_name = request.form['prof']
+        prof = Prof.query.filter_by(name=prof_name).first()
+        courses.prof = prof
+        # update the database
+        db.session.commit()
+        return redirect(url_for('get_courses'))
 
 
 if __name__ == '__main__':
